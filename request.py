f321a138a1f1d7abc84b88f834dde1b1100c6cd0
aditya agrahari <48546730+GAREEeB@users.noreply.github.com>
2019-08-29 00:24:39 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
+r = requests.post(url,json={'step_count':2, 'calories_burned':9, 'hours_of_sleep':6})
 
 print(r.json())
